cc7006e1bc6f17e8da43cdf81a670f95e7cbb86c
DanaKeli <79215094+DanaKeli@users.noreply.github.com>
2021-07-14 23:48:26 +0300

    feat: complete subtask1

--- src/main/kotlin/subtask1/Combinator.kt
+++ src/main/kotlin/subtask1/Combinator.kt
@@ -2,8 +2,39 @@ package subtask1
 
 class Combinator {
 
-    // TODO: Complete the following function
+
     fun checkChooseFromArray(array: Array<Int>): Int? {
-        throw NotImplementedError("Not implemented")
+
+        val p = array[0]
+        val c = array[1]
+
+        if (array.size != 2) return null
+        if (array[0] <= array[1]) return 1
+        if (array[0] < 0 || array[1] <= 0) return null
+
+        for (x in 2 until c) {
+            if (combinations(x, c) == p) {
+                return x
+            }
+        }
+        return null
+
+    }
+
+    private fun combinations(x: Int, c: Int): Int {
+        var a: Long = 1
+        for (i in (c - x +1)..c) {
+            a *= i
+        }
+
+        var b: Long = 1
+        for (i in 2..x) {
+            b *= i
+        }
+        return if (a % b == 0L) {
+            (a/b).toInt()
+        } else {
+            0
+        }
     }
 }
